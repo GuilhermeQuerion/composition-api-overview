19b4508c8b41415f024198a03a6bd1512b6dc1c6
GuilhermeQuerion <guilhermequerion@icloud.com>
2022-04-27 14:56:32 -0300

    primeiro commit

---
+++ src/components/ContadorVue.vue
@@ -0,0 +1,38 @@
+<template>
+  <p>Initial value: {{ initialValue }}</p>
+  <p>Atual value: {{count}} </p>
+  <p>triple value: {{triple}} </p>
+  <button @click="add"> Add</button>
+</template>
+
+<script>
+import { ref, computed, watch, onMounted } from "vue";
+
+export default {
+  name: "contador-vue",
+  props: {
+    initialValue: Number,
+  },
+  setup(props) {
+    const count = ref(props.initialValue);
+    const triple = computed(() => count.value *3);
+
+    watch(count, (newValue, oldValue) => {
+      console.log(`new value: ${newValue} old value: ${oldValue}`)
+    });
+
+    onMounted(() =>{
+      console.log('on Monted')
+    });
+
+    const add = () => {
+      count.value++;
+    };
+
+    return { count, add, triple };
+  },
+};
+</script>
+
+<style scoped>
+</style>
